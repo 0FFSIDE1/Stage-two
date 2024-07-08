e358eacdc01e39c7751d14af390e97755ac154fa
OFFSIDE <offsideint@gmail.com>
2024-07-08 18:12:57 +0100

    updated for vercel

--- stageTwo/settings.py
+++ stageTwo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kly6e4bjb5o6_c8u8kxnwnevxo=1_!8+_j438sv)f6+6&5hbvv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stage-two-nu.vercel.app','df80-102-88-68-44.ngrok-free.app','127.0.0.1', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['stage-two-nu.vercel.app','df80-102-88-68-44.ngrok-free.app','127.0.0.1', '127.0.0.1:8000', '.now.sh', '.vercel.app']
 
 
 # Application definition
@@ -154,9 +154,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- stageTwo/urls.py
+++ stageTwo/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from stageTwo import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('App.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
