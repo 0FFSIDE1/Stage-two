950f9d8d270f1505a3901267da19415ea0fa2e95
OFFSIDE <offsideint@gmail.com>
2024-07-08 14:31:41 +0100

    updated

--- App/views.py
+++ App/views.py
@@ -140,6 +140,7 @@ class OrganisationDetailView(APIView):
             return Response({'status': 'Bad request', 'message': 'You do not have access to this organisation', 'statusCode': 403}, status=status.HTTP_403_FORBIDDEN)
 
 class AddUserToOrganisationView(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request, pk):
         context = {
             'user_Id': "Enter a valid Id"
