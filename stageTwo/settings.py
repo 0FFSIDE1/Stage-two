6847248b19bc4c48200259f4f8855af9bcbd27e4
OFFSIDE <somtoemmanuel6@gmail.com>
2024-07-11 11:51:45 +0100

    Update settings.py

--- stageTwo/settings.py
+++ stageTwo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kly6e4bjb5o6_c8u8kxnwnevxo=1_!8+_j438sv)f6+6&5hbvv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stage-two-nu.vercel.app','stage-iqfrun91r-0ffside1s-projects.vercel.app','71a9-102-88-83-219.ngrok-free.app', '127.0.0.1', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['stage-two-nu.vercel.app','stage-nclzmwfel-0ffside1s-projects.vercel.app','71a9-102-88-83-219.ngrok-free.app', '127.0.0.1', '127.0.0.1:8000']
 
 CSRF_TRUSTED_ORIGINS = ['https://71a9-102-88-83-219.ngrok-free.app']
 # Application definition
